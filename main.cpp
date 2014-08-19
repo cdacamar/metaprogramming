5cceb8e6b5cbd2a763f1218675302fb9f0905a0b
Cameron DaCamara <cdacamar@riemann.cs.nmt.edu>
2014-08-19 17:49:26 -0600

    initial code commit, merge sort

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <merge_sort>
+
+int main() {
+  using ints = std::integer_sequence<int, 8,7,33,6,22,5,55,4,3,2,22,1,0>;
+  ints i;
+
+  // sort the thing
+  std::cout << "\nsorted list: " << compile_sort(i, descending{}) << '\n';
+}
