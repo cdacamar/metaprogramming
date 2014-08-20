24ad030342234373edebca167845e275ece81960
Cameron DaCamara <cdacamar@euclid.cs.nmt.edu>
2014-08-20 04:16:39 +0200

    fixing header include in main and making sort call shorter

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-#include <merge_sort>
+#include <merge_sort.hpp>
 
 int main() {
   using ints = std::integer_sequence<int, 8,7,33,6,22,5,55,4,3,2,22,1,0>;
   ints i;
 
   // sort the thing
-  std::cout << "\nsorted list: " << compile_sort(i, descending{}) << '\n';
+  std::cout << "\nsorted list: " << compile_sort(i) << '\n';
 }
