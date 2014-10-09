15e6e946d7077f144cf512baa456b9a75e0d56ab
Cameron DaCamara <cdacamar@euclid.cs.nmt.edu>
2014-10-09 14:10:26 -0600

    removing tabs because they are the devil...

--- tuple_utilities.hpp
+++ tuple_utilities.hpp
@@ -10,12 +10,12 @@ struct tuple_contains;
 
 template <typename T, typename... Ts>
 struct tuple_contains<T, std::tuple<Ts...>> :
-	std::conditional<
-		std::is_same<
-			std::tuple<typename std::conditional<std::is_same<T, Ts>::value, detail::null, Ts>::type...>,
-			std::tuple<Ts...>
-		>::value,
-		std::false_type,
-		std::true_type
-	>::type
+  std::conditional<
+    std::is_same<
+      std::tuple<typename std::conditional<std::is_same<T, Ts>::value, detail::null, Ts>::type...>,
+      std::tuple<Ts...>
+    >::value,
+    std::false_type,
+    std::true_type
+  >::type
 { };
