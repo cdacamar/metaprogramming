10090ebb069579f84c139c1293be6b20faee148f
Cameron DaCamara <cdacamar@euclid.cs.nmt.edu>
2014-10-09 14:07:41 -0600

    adding tuple utils

---
+++ tuple_utilities.hpp
@@ -0,0 +1,21 @@
+#include <tuple>
+#include <type_traits>
+
+namespace detail {
+	struct null { };
+}
+
+template <typename T, typename Tuple>
+struct tuple_contains;
+
+template <typename T, typename... Ts>
+struct tuple_contains<T, std::tuple<Ts...>> :
+	std::conditional<
+		std::is_same<
+			std::tuple<typename std::conditional<std::is_same<T, Ts>::value, detail::null, Ts>::type...>,
+			std::tuple<Ts...>
+		>::value,
+		std::false_type,
+		std::true_type
+	>::type
+{ };
