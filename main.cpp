3ab5684ee4ad95f1afec87ba1b4aeb91e66952cd
Cameron <dacamara.cameron@gmail.com>
2014-10-10 22:27:38 +0000

    adding extras and all_unique_tuple<>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <merge_sort.hpp>
+#include <tuple_utilities.hpp>
 
 int main() {
   using ints = std::integer_sequence<int, 8,7,33,6,22,5,55,4,3,2,22,1,0>;
@@ -8,4 +9,7 @@ int main() {
 
   // sort the thing
   std::cout << "\nsorted list: " << compile_sort(i) << '\n';
+  std::cout << std::boolalpha << '\n' <<
+    all_unique_tuple<std::tuple<int, char, std::string>>::value << '\n' <<
+    all_unique_tuple<std::tuple<int, char, std::string, signed>>::value << '\n';
 }
--- tuple_utilities.hpp
+++ tuple_utilities.hpp
@@ -1,10 +1,15 @@
+#pragma once
+
 #include <tuple>
 #include <type_traits>
 
+#include <and_bools.hpp>
+
 namespace detail {
-	struct null { };
+struct null { };
 }
 
+// find if a tuple contains T
 template <typename T, typename Tuple>
 struct tuple_contains;
 
@@ -17,5 +22,21 @@ struct tuple_contains<T, std::tuple<Ts...>> :
     >::value,
     std::false_type,
     std::true_type
+  >::type
+{ };
+
+// find if a tuple contains all unique Ts...
+template <typename Tuple>
+struct all_unique_tuple;
+
+template <typename... Ts>
+struct all_unique_tuple<std::tuple<Ts...>> : std::true_type { };
+
+template <typename T, typename... Ts>
+struct all_unique_tuple<std::tuple<T, Ts...>> :
+  std::conditional<
+    and_bools<(!std::is_same<T, Ts>::value)...>::value,
+    all_unique_tuple<std::tuple<Ts...>>,
+    std::false_type
   >::type
 { };
